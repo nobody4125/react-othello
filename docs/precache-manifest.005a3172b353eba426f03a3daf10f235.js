7ac593c8c7c1077e3770b9773275739ef447d2c1
nobody4125 <nobody4125@gmail.com>
2019-05-04 20:10:42 +0900

    GitHub Pagesにデプロイするためにビルドの設定を変更

---
+++ docs/precache-manifest.005a3172b353eba426f03a3daf10f235.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecd574cac6e85bfeee97088a18ade7ca",
+    "url": "./index.html"
+  },
+  {
+    "revision": "53020929f34594f2c8c0",
+    "url": "./static/css/main.9702d177.chunk.css"
+  },
+  {
+    "revision": "245abf76a3dff17921fc",
+    "url": "./static/js/2.de8ba4d2.chunk.js"
+  },
+  {
+    "revision": "53020929f34594f2c8c0",
+    "url": "./static/js/main.743549c9.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
